ed03b45631c2c07c76a984d7c7716761d96d46f0
Fabio Herval <fabioherval@gmail.com>
2020-08-17 23:36:15 -0300

    Estilizando os dados da conta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public options: Array<any> = [
+    { icon: 'person-add-outline', text: 'Indicar amigos' },
+    { icon: 'phone-portrait-outline', text: 'Recarga de celular' },
+    { icon: 'wallet-outline', text: 'Depositar' },
+    { icon: 'options-outline', text: 'Ajustar limite' },
+    { icon: 'help-circle-outline', text: 'Me ajuda' },
+    { icon: 'barcode-outline', text: 'Pagar' },
+    { icon: 'lock-open-outline', text: 'Bloquear cartão' },
+    { icon: 'card-outline', text: 'Cartão virtual' }
+  ];
+
+  public slidesOptions: any = { slidesPerView: 3, freeMode: true };
+
+  public items: Array<any> = [
+    { icon: 'help-circle-outline', text: 'Me ajuda' },
+    { icon: 'person-outline', text: 'Perfil' },
+    { icon: 'cash-outline', text: 'Configurar conta' },
+    { icon: 'card-outline', text: 'Configurar cartão' },
+    { icon: 'phone-portrait-outline', text: 'Configurações do app' }
+  ];
 
   constructor() {}
 
