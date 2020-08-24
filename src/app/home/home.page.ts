d8f62c8db4826549ccb52ba8575488fb1896ab0d
Fabio Herval <fabioherval@gmail.com>
2020-08-24 18:09:24 -0300

    Estilizando os blocos frontais

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Renderer2 } from '@angular/core';
+import { AnimationController, Animation, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('blocks') blocks: any;
+  @ViewChild('background') background: any;
+
   public options: Array<any> = [
     { icon: 'person-add-outline', text: 'Indicar amigos' },
     { icon: 'phone-portrait-outline', text: 'Recarga de celular' },
@@ -27,6 +31,42 @@ export class HomePage {
     { icon: 'phone-portrait-outline', text: 'Configurações do app' }
   ];
 
-  constructor() {}
+  public initialStep: number = 0;
+  private maxTranslate: number;
+  private animation: Animation;
 
+  constructor(
+    private animationCtrl: AnimationController,
+    private platform: Platform,
+    private renderer: Renderer2
+    ) {
+      this.maxTranslate = this.platform.height() - 200;
+    }
+
+    ngAfterViewInit() {
+      this.createAnimation();
+    }
+
+    toggleBlocks() {
+      this.initialStep = this.initialStep === 0 ? this.maxTranslate : 0;
+
+      this.animation.direction(this.initialStep === 0 ? 'reverse' : 'normal').play();
+
+      this.setBackgroundOpacity();
+    }
+
+    createAnimation() {
+      this.animation = this.animationCtrl.create()
+      .addElement(this.blocks.nativeElement)
+      .duration(300)
+      .fromTo('transform', 'translateY(0)', `translateY(${this.maxTranslate}px)`);
+    }
+
+    setBackgroundOpacity() {
+      this.renderer.setStyle(this.background.nativeElement, 'opacity', this.initialStep === 0 ? '0' : '1');
+    }
+
+    fixedBlocks(): boolean {
+      return this.initialStep === this.maxTranslate;
+    }
 }
